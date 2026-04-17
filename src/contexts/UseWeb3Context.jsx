4fd54e343002f0fe9d048a6b804f9ee37b8c07bc
Shahidalmamun <shahidalmamun789@gmail.com>
2026-04-18 01:12:36 +0800

    fix: remove duplicate frontend from root

--- src/contexts/UseWeb3Context.jsx
+++
@@ -1,7 +0,0 @@
-import { Web3Context } from "./CreateWeb3Context";
-import { useContext } from "react";
-
-export const useWeb3Context = () => {
-    return useContext(Web3Context)
-}
-
--- src/routes/routes.jsx
+++
@@ -1,8 +0,0 @@
-import {createBrowserRouter} from 'react-router-dom';
-import Wallet from '../pages/Wallet';
-import Home from '../pages/Home';
-
-export const routes = createBrowserRouter([
-    {path:'/', element: <Wallet />},
-    {path:'/home', element: <Home />}
-])
